2dcd5b83d28b530651058bd1eb52ad9b40cdeb94
swag <swag@fdfdf>
2016-08-11 17:23:46 +0300

    shrek is love shrek is life

--- Ig2/Scripts/Functions.js
+++ Ig2/Scripts/Functions.js
@@ -1,24 +1,18 @@
 ﻿$(document).ready(function () {
-    $('#forward').on('click', function (e) {
-        e.preventDefault();
-        $('#content').load('/Search/Forward');
-    });
-});
-$(document).ready(function () {
     $.ajax({
         url: '/Search/Json/',
         type: 'POST',
         data: { baze: 'usd' },
         success: function (result) {
             var obj = $.parseJSON(result);
-            var dd;
+            var options;
             $.each(obj['rates'], function (key, value) {
-                dd += '<option value=' + value + '>' + key + '</option>';
+                options += '<option value=' + value + '>' + key + '</option>';
             });
-            $("#valz").append(dd);
+            $("#currency").append(options);
         },
         error: function () {
-            alert("error");
+            console.log('Error retrieving json.');
         }
     });
 });
